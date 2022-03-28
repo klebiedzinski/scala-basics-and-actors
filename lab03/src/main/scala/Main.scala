ebd4247ae0c796651fc07158ecf54d692eb09ad1
karol <lebiedzinskik02@gmail.com>
2022-03-28 19:01:27 +0200

    zadania lab03

--- lab03/src/main/scala/Main.scala
+++ lab03/src/main/scala/Main.scala
@@ -19,8 +19,37 @@ def zad6: Unit = {
 }
 
 @main 
-def zad8(arg: Int): Unit = {
-
+def zad7(arg: Int): Unit = {
+   def IsPrime(x: Int): Boolean = {
+    if (x==2) true
+    else {
+      @scala.annotation.tailrec
+      def tailrec(x: Int, akum: Int): Boolean = {
+        if (x%akum == 0) false
+        else if (akum*akum>x) true
+        else tailrec(x,akum+1)
+      }
+      tailrec(x,2)
+    }
+    
+  }
+  println(IsPrime(arg))
 }
+
+@main
+def zad8(arg: Int): Unit = {
+  def ciag(n: Int): Int = {
+    @scala.annotation.tailrec
+    def tailrec(n: Int,akum1: Int,akum2: Int): Int = {
+      if (n==0) akum1
+      else tailrec(n-1,akum2,akum1+akum2)
+    }
+    tailrec(n,2,1)
+  }
+  println(ciag(arg))
+  
+}
+
+
 
 
