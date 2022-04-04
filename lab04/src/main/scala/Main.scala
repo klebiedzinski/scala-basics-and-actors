75fe80c29eebb6787a98053ef09a1f40aa0f4a3a
karol <lebiedzinskik02@gmail.com>
2022-04-05 00:38:04 +0200

    zadania lab04

--- lab04/src/main/scala/Main.scala
+++ lab04/src/main/scala/Main.scala
@@ -17,4 +17,57 @@ def zadanie_10: Unit = {
     println(wynik)
 }
 
+@main
+def zadanie_11: Unit = {
+  def maksimum(l1: List[Double], l2: List[Double]): List[Double] = {
+    @tailrec
+      def tailrec(l1: List[Double], l2: List[Double],akum: List[Double] = List()): List[Double] =(l1,l2) match {
+        case (Nil,Nil) => akum
+        case (Nil,List(_*)) => akum.appendedAll(l2)
+        case (List(_*),Nil) => akum.appendedAll(l1)
+        case (głowa :: ogon , głowa2 :: ogon2) =>
+          if (głowa>głowa2) tailrec(ogon,ogon2,akum :+ głowa)
+          else tailrec(ogon,ogon2,akum :+ głowa2)
+      }
+      tailrec(l1,l2)
+  }
+  val lista1 = List(2.0, -1.6, 3.2, 5.4, -8.4)
+  val lista2 = List(3.3, -3.1, 3.2, -4.1, -0.4, 5.5)
+  val wynik = maksimum(lista1, lista2) // ==> List(3.3, -1.6, 3.2, 5.4, -0.4, 5.5)
+  println(wynik)
+}
+
+@main
+def zadanie_12: Unit = {
+    def usun[A](l: List[A], el: A): List[A] = {
+    def tailrec(l: List[A], el: A,akum: List[A]): List[A] = l match {
+      case Nil => akum
+      case head :: tail => 
+        if (head!=el) tailrec(tail,el,akum :+ head)
+        else tailrec(tail,el,akum)
+    }
+    tailrec(l,el,Nil)
+  }
+    val lista = List(2, 1, 4, 1, 3, 3, 1, 2)
+    val wynik = usun(lista, 1) // ==> List(2, 4, 3, 3, 2).
+    println(wynik)
+}
+
+@main
+def zadanie_13: Unit = {
+  def divide[A](l: List[A]): (List[A], List[A]) = {
+    def tailrec(l: List[A], akum1: List[A], akum2: List[A],index: Int ): (List[A], List[A]) = l match {
+      case Nil => (akum1, akum2)
+      case głowa :: ogon =>
+        if (index%2==0) tailrec(ogon,akum1 :+ głowa,akum2, 1+ index)
+        else tailrec(ogon,akum1,akum2 :+ głowa,1 + index)
+    }
+    tailrec(l,Nil,Nil,0)
+  }
+    val lista = List(1, 3, 5, 6, 7)
+     
+    println(divide(lista))
+}
+
+
 
