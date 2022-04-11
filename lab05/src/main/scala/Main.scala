0cdb719319614a8e445c8de599fa70ad77e16cc4
karol <lebiedzinskik02@gmail.com>
2022-04-11 19:51:27 +0200

    zadania lab05

--- lab05/src/main/scala/Main.scala
+++ lab05/src/main/scala/Main.scala
@@ -37,4 +37,34 @@ def zadanie_16: Unit = {
   println(skompresuj(lista)) // ==> List(('a', 2), ('b', 1), ('c', 3), ('a', 2), ('b', 1), ('d', 1))
 }
 
+def isOrdered[T](arr: List[T])(leq: (T, T) => Boolean): Boolean = {
+  @annotation.tailrec
+  def tailrec(arr: List[T])(leq: (T, T) => Boolean): Boolean = arr match {
+    case Nil | List(_) => true
+    case first :: second :: tail if leq(first, second) => tailrec(second :: tail)(leq)
+    case first :: second :: tail => false
+  }
+
+  tailrec(arr)(leq)
+}
+@main
+def zadanie_17(): Unit = {
+  println(isOrdered(List(1, 2, 2, 4))(_ < _))
+  println(isOrdered(List(1, 2, 2, 4))(_ <= _))
+}
+
+
+def applyForAll[A, B](arr: List[A])(func: A => B): List[B] = {
+  @annotation.tailrec
+  def tailrec(arr: List[A], acc: List[B] = Nil)(func: A => B): List[B] = arr match {
+    case Nil => acc.reverse
+    case head :: tail => tailrec(tail, func(head) :: acc)(func)
+  }
+
+  tailrec(arr)(func)
+}
+@main
+def zadanie_18(): Unit = {
+  println(applyForAll(List(1, 3, 5))(n => n + 3))
+}
 
