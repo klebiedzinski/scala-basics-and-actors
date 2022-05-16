851cc4297c55994f01f7add965ee0986b1638e7b
karol <lebiedzinskik02@gmail.com>
2022-05-16 13:09:17 +0200

    fixes

--- lab08/lab08/src/main/scala/Main.scala
+++ lab08/lab08/src/main/scala/Main.scala
@@ -11,3 +11,23 @@ def zad_30: Unit ={
   println(MasterMind(secret,guess))
  
 }
+
+def wynikZawodnika(wyniki: List[Any], imie: String, nazwisko: String): (Double, Double) = {
+  println(wyniki.filter( list === (imie, nazwisko, _, _)))
+  
+}
+@main
+def zad_31: Unit = {
+  val wyniki: List[List] = (
+  ("Maciej","Maziuk",3,12),
+  ("Maciej","Maziuk",6,6),
+  ("Maksym","Malek",3,5),
+  ("Maciej","Maziuk",1,20),
+  ("Karol", "Lebiedzinski", 5,8),
+  ("Karol", "Lebiedzinski", 5,20),
+  )
+  wynikZawodnika(wyniki,"Maksym","Malek")
+}
+
+
+
